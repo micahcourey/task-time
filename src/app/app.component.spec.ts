fbf0d0e16d4218afc598cdbaa3f73fd95d902c9f
micahcourey <micahjcourey@gmail.com>
2017-05-11 21:43:07 -0700

    Add project files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,16 +17,23 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title 'Task Time!'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('Task Time!');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('Task Time!');
+  }));
+
+  it(`should render a initial task 'Get started'`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled =  fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain('Get started');
   }));
 });
